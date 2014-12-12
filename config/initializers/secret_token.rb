25d06bd2d4cb52b52240c1d7c46347fbd67df8a6
saasbook <saasbook@saasbook.(none)>
2014-12-12 16:15:41 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = 'dafe77c10d95f97142b809830f8c1dcaba535264ac303c096ba3b36e4bc6185c3a1d63f1c457d57d7ab70c3f5dfa8830ec2dd8599c4255b7700739b694e4dd7e'

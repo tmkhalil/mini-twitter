6cc0f696d8b478039f693973c9c0bda7fef218ef
saasbook <saasbook@saasbook.(none)>
2014-12-25 02:29:51 +0200

    last stable 2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :uer_id
+  attr_accessible :content, :user_id
   validates_presence_of :user_id
   validates :content, :length => {:minimum =>1}
   belongs_to :user
-  default_scope order('created_at DESC')
+  default_scope order('posts.created_at DESC')
 end

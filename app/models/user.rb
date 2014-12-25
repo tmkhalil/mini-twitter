04714bb3c25a01034db02b1423464f9c3d3f4784
saasbook <saasbook@saasbook.(none)>
2014-12-25 02:34:14 +0200

    following DB added

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts, :dependent => :destroy
   has_many :relationships, foreign_key: "follower_id", :dependent => :destroy
-  has_many :followed_users, through: => :relationships, source: => :followed 
+  has_many :followed_users, :through => :relationships, :source => :followed 
   has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", :dependent => :destroy
-  has_many :followers, through: => :reverse_relationships, source: => :follower
+  has_many :followers, :through => :reverse_relationships, :source => :follower
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
